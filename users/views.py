09cdfeaec96327ef0a07b506afe608f7aac32f33
Akash-nath29 <anath5440@gmail.com>
2025-02-20 12:22:43 +0530

    Redirect user to home after successful login instead of material list

--- users/views.py
+++ users/views.py
@@ -30,7 +30,7 @@ def user_login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('material-list')
+            return redirect('home')
         else:
             messages.error(request, 'Invalid username or password!')
     
